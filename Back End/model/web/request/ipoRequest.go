9e1165f237ca5ac6c034b7efdfb875ae726dfea2
Richard Sugiharto <richardsugiharto0@gmail.com>
2025-12-12 00:06:48 +0700

    Move Backend to separate repository

--- Back End/model/web/request/ipoRequest.go
+++
@@ -1,8 +0,0 @@
-package request
-
-type Filter struct {
-	FilterName  string `json:"filter_name"`
-	FilterValue string `json:"filter_value"`
-	Symbol      string `json:"symbol"`
-	FilterType  string `json:"filter_type"`
-}
